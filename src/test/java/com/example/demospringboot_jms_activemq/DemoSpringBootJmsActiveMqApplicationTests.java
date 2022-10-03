81ba19802ba447cdb907d0f2a1484f82a6f56086
Zoe <zoe.vo@emagegroup.com>
2022-10-03 13:27:52 +0700

    start project

---
+++ src/test/java/com/example/demospringboot_jms_activemq/DemoSpringBootJmsActiveMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringboot_jms_activemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootJmsActiveMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
